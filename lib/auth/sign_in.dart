f487756b9ff2ad8c481417979aac605e06ff46f3
Daham Adikari <dahamadikary0712@gmail.com>
2024-08-21 03:32:58 +0530

    Role based sign up done

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:test_4/auth/log_in.dart';
-import 'package:test_4/auth/home_page.dart';
-
-//import '../LogIn_SignIn_Common/signin_common.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:test_4/pages/Driver/buslist_driver.dart';
+import 'package:test_4/pages/passenger/searchpage_passenger.dart';
+import 'home_page.dart';
+import 'log_in.dart';
 import 'constants/colors.dart';
 import 'constants/image_strings.dart';
 import 'constants/sizes.dart';
 
-// class PassengerSingInScreen extends StatelessWidget {
-//   const PassengerSingInScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final size = MediaQuery.of(context).size;
-//     return signin_common(
-//       onTap1: (){ Get.to(()=>const PassengerLogInScreen());},
-//       onTap2: (){},
-//       size: size,
-//       text1: ("Create Your Profile to Start Your Journey"),
-//     );
-//   }
-// }
-
 class SignInScreen extends StatelessWidget {
-  const SignInScreen({super.key});
+  final String role;
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  SignInScreen({super.key, required this.role});
+
+  Future<void> _signUp() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Create user with email and password
+        UserCredential userCredential =
+            await _auth.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+        );
+
+        // Store user details in Firestore
+        await _firestore
+            .collection(role.toLowerCase())
+            .doc(userCredential.user!.uid)
+            .set({
+          'full_name': _nameController.text.trim(),
+          'email': _emailController.text.trim(),
+          'role': role,
+          'userID': userCredential.user!.uid,
+        });
+
+        // Navigate to the appropriate page based on the role
+        if (role.toLowerCase() == 'passenger') {
+          Get.to(() => SearchBusPage());
+        } else if (role.toLowerCase() == 'driver') {
+          Get.to(() => BusListPageDriver());
+        }
+      } catch (e) {
+        // Handle errors
+        Get.snackbar("Error", e.toString());
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,10 +90,12 @@ class SignInScreen extends StatelessWidget {
                 padding:
                     const EdgeInsets.symmetric(vertical: tDefaultSize - 10),
                 child: Form(
+                  key: _formKey,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       TextFormField(
+                        controller: _nameController,
                         decoration: InputDecoration(
                           label: Text("Full Name",
                               style: TextStyle(color: tPrimaryColor)),
@@ -76,9 +108,16 @@ class SignInScreen extends StatelessWidget {
                                 BorderSide(width: 2.0, color: tPrimaryColor),
                           ),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your full name';
+                          }
+                          return null;
+                        },
                       ),
                       SizedBox(height: 12),
                       TextFormField(
+                        controller: _emailController,
                         decoration: InputDecoration(
                           label: Text("E-mail",
                               style: TextStyle(color: tPrimaryColor)),
@@ -91,29 +130,20 @@ class SignInScreen extends StatelessWidget {
                                 BorderSide(width: 2.0, color: tPrimaryColor),
                           ),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
                       ),
                       SizedBox(height: 12),
                       TextFormField(
+                        controller: _passwordController,
                         decoration: InputDecoration(
                           label: Text("Password",
                               style: TextStyle(color: tPrimaryColor)),
                           hintText: "Enter Your Password",
-                          prefixIcon:
-                              Icon(Icons.fingerprint_rounded, color: tPrimaryColor),
-                          border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 2.0, color: tPrimaryColor),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          label: Text("Confirm Password",
-                              style: TextStyle(color: tPrimaryColor)),
-                          hintText: "ReEnter Your Password",
                           prefixIcon: Icon(Icons.fingerprint_rounded,
                               color: tPrimaryColor),
                           border: OutlineInputBorder(),
@@ -122,14 +152,45 @@ class SignInScreen extends StatelessWidget {
                                 BorderSide(width: 2.0, color: tPrimaryColor),
                           ),
                         ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 12),
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          label: Text("Confirm Password",
+                              style: TextStyle(color: tPrimaryColor)),
+                          hintText: "Re-enter Your Password",
+                          prefixIcon: Icon(Icons.fingerprint_rounded,
+                              color: tPrimaryColor),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 2.0, color: tPrimaryColor),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your password';
+                          }
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
                       ),
                       SizedBox(height: 50),
                       SizedBox(
                         width: double.infinity,
                         child: ElevatedButton(
-                          onPressed: () {
-                            Get.to(() => const HomePage());
-                          },
+                          onPressed: _signUp, // Call the sign-up method
                           style: OutlinedButton.styleFrom(
                             shape: RoundedRectangleBorder(),
                             backgroundColor: Colors.black87,
@@ -192,4 +253,3 @@ class SignInScreen extends StatelessWidget {
     );
   }
 }
-//git
