187dfb88a477f29f07e04c81308fb5b8ca445579
Rashmi-kavindya <kavidissanayaka20@gmail.com>
2024-08-30 19:14:09 +0530

    Add a comment to the main

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:test_4/pages/UserSelectionPage.dart';
 import 'package:test_4/pages/admin.dart';
 //import 'package:test_4/pages/Useless/map_page.dart';
 //draft pull
+//rashmipull
 
 void main() async {
   WidgetsFlutterBinding
